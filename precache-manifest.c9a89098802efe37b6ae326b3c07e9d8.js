e83e48e88120e6ef1a407d607f84e97c03e22bbf
Md. Abu Raihan Srabon <m.arsrabon@gmail.com>
2023-08-04 02:38:52 +0600

    Updates

---
+++ precache-manifest.c9a89098802efe37b6ae326b3c07e9d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d94fb35f4844f5cd57f6",
+    "url": "/static/css/main.0a7db065.chunk.css"
+  },
+  {
+    "revision": "d94fb35f4844f5cd57f6",
+    "url": "/static/js/main.eac2bfc1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3e4ba0ae3183cd7031cf",
+    "url": "/static/js/2.66f691a8.chunk.js"
+  },
+  {
+    "revision": "79f86f210df3db2147d51c68ee2c41c1",
+    "url": "/index.html"
+  }
+];
